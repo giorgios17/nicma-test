2715d4814e44ecfc80d87b3e1ae1ddbcabd2e203
giorgios17 <giosur@live.it>
2022-07-19 14:53:09 +0200

    commenti

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -11,7 +11,9 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
+        //richiamo i post per popolare la tabella "post" del db
         $posts = json_decode(file_get_contents('https://jsonplaceholder.typicode.com/posts'));
+        //ciclo i post e li inserisco nel db
         foreach ($posts as $post) {
             Post::create([
                 'userid' => $post->userId,
